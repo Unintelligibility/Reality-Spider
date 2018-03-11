eb05493320a5cb1754058480d0e3661081fb9471
cccyf <731767381@qq.com>
2018-03-11 18:41:19 +0800

    <delete>:<remove encode>

--- zaker-crawler/zakerNews/zakerNews/spiders/zaker.py
+++ zaker-crawler/zakerNews/zakerNews/spiders/zaker.py
@@ -25,32 +25,31 @@ class ZakerCrawlSpider(CrawlSpider):
 
     def parse_item(self, response):
         news = News()
-        title = response.xpath('//h1/text()')[0].extract().encode('utf-8')
+        title = response.xpath('//h1/text()')[0].extract()
         pic_part=response.xpath('//div[contains(@id,"id_imagebox_0")]/div/img/@data-original')
         if(len(pic_part) is 0):
             return
-        picture = pic_part[0].extract().encode('utf-8')
-        source = response.xpath('//span[contains(@class,"auther")]/text()')[0].extract().encode('utf-8')
+        picture = pic_part[0].extract()
+        source = response.xpath('//span[contains(@class,"auther")]/text()')[0].extract()
         time = response.xpath('//span[contains(@class,"time")]/text()')[0].extract()
         time = re.sub(r'[0-9]*分钟前',str(getDayBefore(daynum=0)),time)
         time = re.sub(r'[0-9]*小时前',str(getDayBefore(daynum=0)),time)
         time = re.sub(r'昨天',str(getDayBefore(daynum=1)),time)
         time = re.sub(r'前天',str(getDayBefore(daynum=2)),time)
-        time = time.encode('utf-8')
 
-        content_text = (response.xpath('//h1/text()')[0].extract()+'.'.join(response.xpath('//div[contains(@class,"article_content")]//text()').extract())).encode('utf-8')
+        content_text = (response.xpath('//h1/text()')[0].extract()+'.'.join(response.xpath('//div[contains(@class,"article_content")]//text()').extract()))
+        
         header_html = response.xpath('//div[contains(@class,"article_header")]')[0].extract()
         header_html = re.sub(r'<span class="time">[0-9]*分钟前</span>','<span class="time">'+str(getDayBefore(daynum=0))+'</span>',header_html)
         header_html = re.sub(r'<span class="time">[0-9]*小时前</span>','<span class="time">'+str(getDayBefore(daynum=0))+'</span>',header_html)
         header_html = re.sub(r'<span class="time">昨天</span>','<span class="time">'+str(getDayBefore(daynum=1))+'</span>',header_html)
         header_html = re.sub(r'<span class="time">前天</span>','<span class="time">'+str(getDayBefore(daynum=2))+'</span>',header_html)
-        header_html = header_html.encode('utf-8')
 
-        content_html = response.xpath('//div[contains(@class,"article_content")]')[0].extract().replace('data-original','src').encode('utf-8')
+        content_html = response.xpath('//div[contains(@class,"article_content")]')[0].extract().replace('data-original','src')
         content_html = header_html+content_html;
 
-        news_type = response.xpath('//ol[contains(@class,"breadcrumb")]/li//text()')[-2].extract().encode('utf-8')
-        news_tags = (';'.join(response.xpath('//div[contains(@class,"article_more")]/a//text()').extract())).encode('utf-8')
+        news_type = response.xpath('//ol[contains(@class,"breadcrumb")]/li//text()')[-2].extract()
+        news_tags = (';'.join(response.xpath('//div[contains(@class,"article_more")]/a//text()').extract()))
     
         news["title"]=title
         news["url"]=response.url
